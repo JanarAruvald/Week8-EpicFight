2e74b7efb45c738ea8f18afff043569a4a0c1cd1
Opilane TTHK <opilane@tthk.ee>
2021-10-26 10:05:16 +0300

    EpicFight

--- Program.cs
+++ Program.cs
@@ -8,15 +8,58 @@ namespace EpicFight
         {
             string hero = PickHero();
             string villain = PickVillain();
+            int HeroHP = GenerateHP(hero);
+            int villainHP = GenerateHP(villain);
+
             Console.WriteLine($"{hero} will fight {villain}.");
 
-            string heroWeapon = PickWeapon();
-            string villainWeapon = PickWeapon();
+            string heroWeapon = PickWeaponHero();
+            string villainWeapon = PickWeaponVillain();
             Console.WriteLine($"{hero} picked {heroWeapon}. {villain} picked {villainWeapon}.");
+
+            while(HeroHP > 0 && villainHP > 0)
+            {
+                HeroHP = HeroHP - Hit(villain, hero, villainWeapon);
+                villainHP = villainHP - Hit(hero, villain, heroWeapon);
+            }
+
+            if(HeroHP <= 0)
+            {
+                Console.WriteLine("Dark side wins!");
+            }
+            else
+            {
+                Console.WriteLine($"{hero} saves the day!");
+            }
+
+        }
+        private static int Hit(string CharacterOne, string CharacterTwo, string someWeapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, someWeapon.Length / 2);
+
+            Console.WriteLine($"{CharacterOne} hit {strike}.");
+
+            if(strike == someWeapon.Length / 2)
+            {
+                Console.WriteLine($"Awesome! {CharacterOne} made a critical hit!");
+            }
+            else if(strike == 0)
+            {
+                Console.WriteLine($"{CharacterTwo} dodged the attack!");
+            }
+
+            return strike;
+        }
+
+        private static int GenerateHP(string someName)
+        {
+            Random rnd = new Random();
+            return rnd.Next(someName.Length, someName.Length + 10);
         }
         private static string PickHero()
         {
-            string[] superHeroes = { "Luke Skywalker", "Batman", "Spiderman", "Spongebob", "Mike Wazovski" };
+            string[] superHeroes = { "Batman", "Spiderman", "Spongebob", "Mike Wazovski" };
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, superHeroes.Length);
 
@@ -24,28 +67,28 @@ namespace EpicFight
         }
         private static string PickVillain()
         {
-            string[] villain = { "Joker", "Darth Vader", "Voldemort", "Hitler", "Thanos" };
+            string[] villain = { "Joker", "Darth Vader", "Ahmed", "Hitler", "Thanos" };
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, villain.Length);
 
             return villain[randomIndex];
         }
-        private static string PickWeapon()
+        private static string PickWeaponHero()
         {
-            string[] heroWeapon = {"kööginuga", "kummipeenis", "auto", "Nuclear Bomb" };
+            string[] heroWeapon = {"kööginuga", "kaka", "klaaspudeli põhi", "Nuclear Bomb"};
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, heroWeapon.Length);
 
             return heroWeapon[randomIndex];
-
-            string[] villainWeapon = {"Saw", "Klaviatuur"};
+        }
+        private static string PickWeaponVillain()
+        {
+            string[] villainWeapon = { "Saw", "Klaviatuur", "rusikad", "banana" };
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, villainWeapon.Length);
 
             return villainWeapon[randomIndex];
-
         }
-
 
 
 
